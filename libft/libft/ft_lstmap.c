342e6ecea948e7c666c44adedaf7b62c0ed87053
donglee <donglee@student.42seoul.kr>
2020-03-12 21:57:41 +0900

    lstmap debug

--- libft/libft/ft_lstmap.c
+++ libft/libft/ft_lstmap.c
@@ -1,23 +1,138 @@
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	t_list *ret;
-	t_list *node;
+// void	*ffunc(void *content)
+// {
+// 	// printf("ffunc content: %p\n", content);
+// 	return content;
+// }
 
-	if (lst && f && del)
-	{
-		ret = NULL;
-		while (lst)
-		{
-			if ((node = ft_lstnew(f(lst->content))) == NULL)
-			{
-				ft_lstclear(&ret, del);
-				return (NULL);
-			}
-			ft_lstadd_back(&ret, node);
-			lst = lst->next;
-		}
-	}
-	return (ret);
+// void	dfunc(void *content)
+// {
+// 	int a = 1;
+// 	// printf("dfunc content: %p\n", content);
+// 	if (content)
+// 		a += 1;
+// }
+
+// t_list  *ft_lstnew(void *content)
+// {
+// 	t_list *ret;
+
+// 	if ((ret = (t_list *)malloc(sizeof(ret))) == 0)
+// 		return (NULL);
+// 	ret->next = NULL;
+// 	ret->content = content;
+// 	return (ret);
+// }
+
+// void	ft_lstdelone(t_list *lst, void (*del)(void *))
+// {
+// 	if (lst)
+// 	{
+// 		if (del)
+// 			del(lst->content);
+// 		free(lst);
+// 	}
+// }
+
+// void	ft_lstclear(t_list **lst, void (*del)(void *))
+// {
+// 	t_list *tmp;
+
+// 	if (lst && *lst && del)
+// 	{
+// 		while (*lst)
+// 		{
+// 			tmp = (*lst)->next;
+// 			ft_lstdelone(*lst, del);
+// 			(*lst) = tmp;
+// 		}
+// 		*lst = NULL; 
+// 	}
+// }
+
+// void	ft_lstadd_back(t_list **lst, t_list *new)
+// {
+// 	t_list *tmp;
+
+// 	if (lst)
+// 	{
+// 		tmp = *lst;
+// 		if (!tmp)
+// 			*lst = new;
+// 		else
+// 		{
+// 			while (tmp->next)
+// 				tmp = tmp->next;
+// 			tmp->next = new;
+// 		}
+// 	}
+// }
+
+// t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+// {
+// 	t_list *ret;
+// 	t_list *node;
+
+// 	ret = NULL;
+// 	if (lst && f && del)
+// 	{
+// 		while (lst)
+// 		{
+// 			if ((node = ft_lstnew(f(lst->content))) == NULL)
+// 			{
+// 				ft_lstclear(&ret, del);
+// 				return (NULL);
+// 			}
+// 			ft_lstadd_back(&ret, node);
+// 			lst = lst->next;
+// 		}
+// 	}
+// 	return (ret);
+// }
+
+// t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+// {
+// 	t_list *ret;
+// 	t_list *tmp;
+
+// 	if (lst && f && del)
+// 	{
+// 		ret = tmp;
+// 		while (lst)
+// 		{
+// 			tmp = f(lst->content);
+// 			lst = lst->next;
+// 		}
+// 	}
+// }
+
+int main()
+// {
+// 	t_list one;
+// 	t_list two;
+// 	t_list three;
+
+// 	t_list *pone = &one;
+// 	t_list *ptwo = &two;
+// 	t_list *pthree = &three;
+
+// 	pone->next = ptwo;
+// 	ptwo->next = pthree;
+// 	pthree->next = NULL;
+
+// 	printf("pone: %p\n", pone);
+// 	printf("ptwo: %p\n", ptwo);
+// 	printf("pthree: %p\n", pthree);
+// 	void *(*f)(void *) = ffunc;
+// 	void (*del)(void *) = dfunc;
+
+// 	t_list *result = ft_lstmap(pone, f, del);
+// 	while (result)
+// 	{
+// 		printf("result: %p\n", result);
+// 		result = result->next;
+// 	}
+/* pointer test 를 해보자 시펄! */
+	return 0;
 }
