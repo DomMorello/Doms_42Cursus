ee7a4ca3ce8dbbe34488e815a9e269381c9f2310
DomMorello <zuzudnf@gmail.com>
2020-03-13 20:50:38 +0900

    test

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,19 +1,20 @@
 #include "ft_printf.h"
 
-int	ft_printf(const char *format, ...)
-{
-	va_list ap;
+// int	ft_printf(const char *format, ...)
+// {
+// 	va_list ap;
 
 
-}
+// }
 
 int main()
 {
-	int a = 123; 
-	int b = -123;
-	double c = 1.23456;
-
-	printf("%5.2f\n", c);
-	printf("%5.34f\n", c);
+	// int a = 48;
+	// printf("%-s\n","abc");
+	char a = 'a';
+	// char *s = "abc";
+	void *s = &a;
+	printf("%-20p\n", s);
+	printf("%20p\n", s);
 	return 0;
 }
