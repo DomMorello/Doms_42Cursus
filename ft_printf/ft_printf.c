80673ec862eb81037159573f4164fe6e2cc547c1
DomMorello <zuzudnf@gmail.com>
2020-03-17 12:39:01 +0900

    update

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -9,12 +9,22 @@
 
 int main()
 {
-	// int a = 48;
-	// printf("%-s\n","abc");
-	char a = 'a';
-	// char *s = "abc";
-	void *s = &a;
-	printf("%-20p\n", s);
-	printf("%20p\n", s);
+	int a = 0;
+	int b = 6;
+	int c = 123456789;
+	int d = -6;
+	int e = -123456789;
+	// printf("f: %hhd\n", -12); //9,223,372,036,854,775,807
+	printf("a: %.0d\n", a);
+	printf("a: %0d\n\n", a);
+	printf("b: %5d\n", b);
+	printf("b: %-0d\n\n", b);
+	printf("c: %d\n", c);
+	printf("c: %d\n\n", c);
+	printf("d: %07d\n", d);
+	printf("d: %d\n\n", d);
+	printf("e: %.0d\n", e);
+	printf("e: %d\n\n", e);	
+
 	return 0;
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -3,6 +3,7 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <wchar.h>
 
 
 
