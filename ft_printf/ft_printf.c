efbf310c2f882618b9ab8dbad2b6c47cd35d375a
DomMorello <zuzudnf@gmail.com>
2020-03-20 21:32:32 +0900

    va_arg

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -9,7 +9,15 @@ void	data_init(t_data *data)
 	data->type = '\0';
 	data->width = 0;
 	data->i = 0;
-	// data->len = 0;
+}
+
+int		is_type(char c)
+{
+	if (c == 'c' || c == 's' || c == 'p' ||
+		c == 'd' || c == 'i' || c == 'u' ||
+		c == 'x' || c == 'X' || c == '%')
+		return (TRUE);
+	return (FALSE);
 }
 
 int is_flag(char c)
@@ -26,29 +34,30 @@ int ft_isdigit(int c)
 	return (0);
 }
 
-void	parse_flag(t_data *data)
+void	input_flag(t_data *data, char **ptr)
 {
-	while (*(data->copy) && is_flag(*(data->copy)))
+	while ((**ptr) && is_flag((**ptr)))
 	{
-		if (*(data->copy) == '-')
+		if (**ptr == '-')
 			data->flag[MINUS] = TRUE;
-		if (*(data->copy) == '0')
+		if (**ptr == '0')
 			data->flag[ZERO] = TRUE;
-		data->copy++;
-		data->i++;
+		(*ptr)++;
 	}
 }
 
-void	parse_width(t_data *data)
+void	input_width(t_data *data, char **ptr)
 {
 	int tmp;
 
 	tmp = 0;
-	while (*(data->copy) && (ft_isdigit(*(data->copy)) || *(data->copy) == '*'))
+	while (**ptr && (ft_isdigit(**ptr) || **ptr == '*'))
 	{
-		if (*(data->copy) == '*')
+		if (**ptr == '*')
 		{
 			tmp = va_arg(data->ap, int);
+			printf("tmp %d\n", tmp);
+			/* 버그를 찾았다. 여기서 va_arg를 한 번 불렀으니 두 번 반복하니까 다음으로 넘어가버리는 것이다. ㅈ됐다. ㅅㅂ */
 			if (tmp < 0)
 			{
 				data->flag[MINUS] = TRUE;
@@ -56,57 +65,46 @@ void	parse_width(t_data *data)
 			}
 		}
 		else
-			tmp = *(data->copy) - '0' + (tmp * 10);
-		data->copy++;
-		data->i++;
+			tmp = **ptr - '0' + (tmp * 10);
+		(*ptr)++;
 	}
 	data->width = tmp;
 }
 
-void	parse_precision(t_data *data)
+void	input_precision(t_data *data, char **ptr)
 {
 	int tmp;
 
 	tmp = 0;
-	while (*(data->copy) && (ft_isdigit(*(data->copy)) || *(data->copy) == '*'))
+	while (**ptr && (ft_isdigit(**ptr) || **ptr == '*'))
 	{
-		if (*(data->copy) == '*')
+		if (**ptr == '*')
 			tmp = va_arg(data->ap, int);
 		else
-			tmp = *(data->copy) - '0' + (tmp * 10);
-		data->copy++;
-		data->i++;
+			tmp = **ptr - '0' + (tmp * 10);
+		(*ptr)++;
 	}
 	data->precision = tmp;
 }
 
-int		is_type(char c)
+
+void	input_type(t_data *data, char **ptr)
 {
-	if (c == 'c' || c == 's' || c == 'p' ||
-		c == 'd' || c == 'i' || c == 'u' ||
-		c == 'x' || c == 'X' || c == '%')
-		return (TRUE);
-	return (FALSE);
+	if (is_type(**ptr))
+		data->type = **ptr;
+	(*ptr)++;
 }
 
-void	parse_type(t_data *data)
+void	input_data(t_data *data, char **ptr)
 {
-	if (is_type(*(data->copy)))
-		data->type = *(data->copy);
-	data->copy++;
-	data->i++;
-}
-void	input_data(t_data *data)
-{
-	parse_flag(data);
-	parse_width(data);
-	if (*(data->copy) == '.')
+	input_flag(data, ptr);
+	input_width(data, ptr);
+	if (**ptr == '.')
 	{
-		data->copy++;
-		data->i++;
-		parse_precision(data);
+		(*ptr)++;
+		input_precision(data, ptr);
 	}
-	parse_type(data);
+	input_type(data, ptr);
 }
 
 void	get_len(t_data *data)
@@ -194,19 +192,67 @@ int		exception_data(t_data *data, const char *format)
 	return (TRUE);
 }
 
-// void	print_data(t_data *data)
-// {
-// 	while (*(data->print))
-// 	{
-// 		if (*(data->print) == '%')
-// 		{
-// 			data_init(data);
-// 			data->print++;
-// 			input_data(data);
-// 		}
-// 	}
+void	print_c(t_data *data, char ret)
+{
+	if (data->flag[MINUS] == TRUE)
+	{
+		write(1, &ret, 1);
+		while((((data->width)--) - 1) > 0)
+			write(1, " ", 1);
+	}
+	else
+	{
+		while ((((data->width)--) - 1) > 0)
+			write(1, " ", 1);
+		write(1, &ret, 1);
+	}
+}
 
-// }
+void	print_data(t_data *data, char ret)
+{
+	if (data->type == 'c')
+		print_c(data, ret);
+	// if (data->type == 's')
+	// 	print_s(data);
+	// if (data->type == 'p')
+	// 	print_p(data);
+	// if (data->type == 'd' || data->type == 'i')
+	// 	print_di(data);
+	// if (data->type == 'u')
+	// 	print_u(data);
+	// if (data->type == 'x')
+	// 	print_x(data);
+	// if (data->type == 'X')
+	// 	print_X(data);
+	// if (data->type == '%')
+	// 	print_perc(data);	
+}
+
+void	move_to_print(t_data *data)
+{
+	char ret;
+
+	ret = '\0';
+	while (*(data->print))
+	{
+		if (*(data->print) == '%')
+		{
+			ret = va_arg(data->ap, int);
+			data_init(data);
+			data->print++;
+			input_data(data, &data->print);
+			// printf("type: %c\n", data->type);
+			// printf("width: %d\n", data->width);
+			print_data(data, ret);
+		}
+		else
+		{
+			write(1, &*data->print, 1);
+			data->print++;
+		}
+	}
+
+}
 
 int		parse_data(t_data *data, const char *format)
 {
@@ -216,9 +262,12 @@ int		parse_data(t_data *data, const char *format)
 		{
 			data_init(data);
 			data->copy++;
-			input_data(data);
+			input_data(data, &data->copy);
 			if (exception_data(data, format) == ERROR)
 				return (ERROR);
+			// printf("flag minus %d\n", data->flag[MINUS]);
+			// printf("flag zero %d\n", data->flag[ZERO]);
+			// printf("flag width %d\n", data->width);
 		}
 		else
 		{
@@ -226,7 +275,7 @@ int		parse_data(t_data *data, const char *format)
 			data->len++;
 		}
 	}
-	// print_data(data);
+	move_to_print(data);
 	return (TRUE);
 }
 
