401a6320626c08586c061cc1c5d28fc7387ea993
donglee <donglee@student.42seoul.kr>
2020-04-10 17:08:47 +0900

    printf.h

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -40,12 +40,12 @@ typedef struct	s_data
 }				t_data;
 
 /* this part must be deleted after making makefile */
-int			ft_get_digits(long long nbr);
-int ft_isdigit(int c);
-char		*ft_putnbr_base(unsigned long nbr, char *base);
-void 		ft_putnbrl_fd(long long n, int fd);
-size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t ft_strlen(const char *s);
+// int			ft_get_digits(long long nbr);
+// int ft_isdigit(int c);
+// char		*ft_putnbr_base(unsigned long nbr, char *base);
+// void 		ft_putnbrl_fd(long long n, int fd);
+// size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);
+// size_t ft_strlen(const char *s);
 
 
 void		data_init(t_data *data);
