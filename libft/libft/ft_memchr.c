462dc1d02cbddf40d5d9608321d5f3e6af1f0fd7
donglee <donglee@student.42seoul.kr>
2020-04-10 13:53:29 +0900

    memchr

--- libft/libft/ft_memchr.c
+++ libft/libft/ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: donglee <donglee@student.42.fr>            +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/07 18:14:18 by donglee           #+#    #+#             */
-/*   Updated: 2020/04/07 18:14:25 by donglee          ###   ########.fr       */
+/*   Updated: 2020/04/10 13:52:58 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	while (n-- && *(unsigned char *)s)
+	while (n--)
 	{
 		if (*(unsigned char *)s++ == (unsigned char)c)
 			return ((void *)(s - 1));
