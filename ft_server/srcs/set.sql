9da4799376e1512bf2c49191dad6f41d1ceade83
DomMorello <zuzudnf@gmail.com>
2020-04-16 18:12:47 +0900

    test

--- ft_server/srcs/set.sql
+++ ft_server/srcs/set.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY '1234';
 UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='root';
 FLUSH PRIVILEGES;
--- ft_server/srcs/wp-config.php
+++ ft_server/srcs/wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '1234' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
