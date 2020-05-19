311d26cc235a08ead5bd71454cf49f19c1fb373c
DomMorello <zuzudnf@gmail.com>
2020-05-19 16:56:25 +0900

    jandi

--- cub3D/test.c
+++ cub3D/test.c
@@ -10,25 +10,6 @@ int main()
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "domMorello");
 
-	int a = 0;
-	int b = 0;
-	int c = 100;
-	int d = 100;
-
-	while (b < 50)
-	{
-		a = 0;
-		d = 100;
-		while (a < 100)
-		{
-			mlx_pixel_put(mlx_ptr, win_ptr, c, d, 0255);
-			a++;
-			d++;
-		}
-		c++;
-		b++;
-	}
-
 	mlx_loop(mlx_ptr);
 	return (0);
 }
