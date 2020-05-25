244876bdc96c865c06aef9d2f543a4c85afd36b7
DomMorello <zuzudnf@gmail.com>
2020-05-25 21:31:39 +0900

    dunno

---
+++ cub3D/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "./main.h"
+
+int main(int argc, char const *argv[])
+{
+    double posX = 22, posY = 12;      //x and y start position
+    double dirX = -1, dirY = 0;       //initial direction vector
+    double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+
+    double time = 0;    //time of current frame
+    double oldTime = 0; //time of previous frame
+
+    for (int x = 0; x <= WIN_WIDTH; x++)
+    {
+        double cameraX = 2 * x / (double)WIN_WIDTH - 1;
+        double rayDirX = dirX + planeX * cameraX; 
+        double rayDirY = dirY + planeY * cameraX; 
+
+        printf("cameraX: %f rayDirX %f rayDirY %f\n", cameraX, rayDirX, rayDirY);
+    }
+    return 0;
+}
