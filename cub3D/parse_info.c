5228b13936e5aabbb76b523579c30694092abb97
DomMorello <zuzudnf@gmail.com>
2020-06-10 18:31:01 +0900

    check extension

--- cub3D/parse_info.c
+++ cub3D/parse_info.c
@@ -7,7 +7,7 @@ int     error(char *str)
     return (ERROR);
 }
 
-int     read_map(char const *argv)
+int     parse_info(char const *argv)
 {
     int fd;
     char *line;
@@ -16,14 +16,17 @@ int     read_map(char const *argv)
         return (error("Error\ncannot open the file"));
     while (get_next_line(fd, &line))
     {
-        
+
     }
 }
 
 int main(int argc, char const *argv[])
 {
     if (argc == 2)
-        read_map(argv[1]);
+    {
+        if (check_argv(argv[1]) == ERROR || parse_info(argv[1]) == ERROR)
+            return (ERROR);
+    }
     else if (argc == 3)
         /* bmp */
     
