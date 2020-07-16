8ae9e940f2d52721c15dec352ba3b2beb3e48498
DomMorello <zuzudnf@gmail.com>
2020-07-16 19:10:49 +0900

    screwed

--- exam_rank_02/get_next_line.c
+++ exam_rank_02/get_next_line.c
@@ -1,5 +1,66 @@
 #include "./get_next_line.h"
 
+int is_newline(char *save, char **line)
+{
+	char *found = NULL;
+	int end = 0;
+	int len = 0;
+
+	if ((found = ft_strchr(save, '\n')))
+	{
+		end = found - save;
+		len = ft_strlen(found);
+		*line = ft_substr(save, 0, end);
+		ft_strlcpy(save, found + 1, len + 1);
+		return 1;
+	}
+	return 0;
+}
+
+char *ft_strjoindel(char *save, char *buf)
+{
+	char *ret  = NULL;
+	int len1 = 0;
+	int len2 = ft_strlen(buf);
+	if (save)
+		len1 = ft_strlen(save);
+	if ((ret = (char *)malloc(sizeof(char) * (len1 + len2 + 1))) == NULL)
+		return (NULL);
+	if (save)
+	{
+		ft_strlcpy(ret, save, len1 + 1);
+		ft_strdel(&save);
+	}
+	ft_strlcpy(ret + len1, buf, len2 + 1);
+	return ret;
+}
+
+int get_next_line(char **line)
+{
+	int buf_size = 10;
+	char buf[buf_size + 1];
+	int ret = 0;
+	static char *save = NULL;
+
+	if (!line)
+		return -1;
+	if (save && is_newline(save, line) == 1)
+		return 1;
+	while ((ret = read(0, buf, buf_size)) > 0)
+	{
+		buf[buf_size] = 0;
+		if (!(save = ft_strjoindel(save, buf)))
+			return -1;
+		if (is_newline(save, line) == 1)
+			return 1;
+	}
+	if (save)
+		*line = ft_strdup(save);
+	else if (ret == -1 || ret == 0)
+		*line = ft_strdup("");
+	ft_strdel(&save);
+	return ret;
+}
 
 int main()
 {
--- exam_rank_02/inter.c
+++ exam_rank_02/inter.c
@@ -1,70 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int ft_strlen(char *s)
-{
-    int len = 0;
-
-    while (s[len])
-        len++;
-    return (len);
-}
-
-void ft_bzero(char *s, int len)
-{
-    int i = 0;
-
-    while (i <= len)
-    {
-        s[i] = 0;
-        i++;
-    }
-}
-
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        write(1, "\n", 1);
-        return 0;
-    }
-    int len1 = ft_strlen(argv[1]);
-    int len2 = ft_strlen(argv[2]);
-    int len = 0;
-    if (len1 >= len2)
-        len = len1;
-    else
-        len = len2;
     
-    char save[len];
-    ft_bzero(save, len);
-    int i = 0;
-    int j = 0;
-    int s_idx = 0;
-    
-    while (argv[1][i])
-    {
-        j = 0;
-        while (argv[2][j])
-        {
-            if (argv[1][i] == argv[2][j])
-            {
-                save[s_idx] = argv[1][i];
-                s_idx++;
-                break ;
-            }
-            j++;
-        }
-        i++;
-    }
-    printf("save: %s\n", save);
-    len = ft_strlen(save);
-    char ret[len];
-    ft_bzero(ret, len);
-    int r_idx = 0;
-    int flag = 0;
-    i = 0;
-
-    // fuck it
     return 0;
 }
