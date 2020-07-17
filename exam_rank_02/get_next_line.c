546705de3a0a5157e856561a133577facea55fc5
DomMorello <zuzudnf@gmail.com>
2020-07-17 17:26:05 +0900

    gnl practice

--- exam_rank_02/get_next_line.c
+++ exam_rank_02/get_next_line.c
@@ -1,5 +1,86 @@
 #include "./get_next_line.h"
 
+int ft_strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return len;
+}
+
+char *ft_strchr(char *s, int c)
+{
+	char *ret = s;
+
+	while (*ret != c)
+	{
+		if (!*ret)
+			return NULL;
+		ret++;
+	}
+	return ret;
+}
+
+char *ft_substr(char *s, int start, int len)
+{
+	int i = 0;
+	char *ret = NULL;
+
+	if ((ret = (char *)malloc(sizeof(char) * len + 1)) == NULL)
+		return NULL;
+	while (i < len && s[i])
+	{
+		ret[i] = s[start];
+		i++;
+		start++;
+	}
+	ret[i] = 0;
+	return ret;
+}
+
+int ft_strlcpy(char *dst, char *src, int size)
+{
+	int i = 0;
+	int s_len = ft_strlen(src);
+	
+	if (size == 0)
+		return s_len;
+	while (i < size - 1 && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = 0;
+	return s_len;
+}
+
+void ft_strdel(char **s)
+{
+	if (s && *s)
+	{
+		free(*s);
+		*s = NULL;
+	}
+}
+
+char *ft_strdup(char *s)
+{
+	int len = ft_strlen(s);
+	int i = 0;
+	char *ret = NULL;
+
+	if ((ret = (char *)malloc(sizeof(char) * len + 1)) == NULL)
+		return NULL;
+	while (s[i])
+	{
+		ret[i] = s[i];
+		i++;
+	}
+	ret[i] = 0;
+	return ret;
+}
+
 int is_newline(char *save, char **line)
 {
 	char *found = NULL;
@@ -19,13 +100,13 @@ int is_newline(char *save, char **line)
 
 char *ft_strjoindel(char *save, char *buf)
 {
-	char *ret  = NULL;
+	char *ret = NULL;
 	int len1 = 0;
 	int len2 = ft_strlen(buf);
 	if (save)
 		len1 = ft_strlen(save);
 	if ((ret = (char *)malloc(sizeof(char) * (len1 + len2 + 1))) == NULL)
-		return (NULL);
+		return NULL;
 	if (save)
 	{
 		ft_strlcpy(ret, save, len1 + 1);
@@ -37,9 +118,9 @@ char *ft_strjoindel(char *save, char *buf)
 
 int get_next_line(char **line)
 {
+	int ret = 0;
 	int buf_size = 10;
 	char buf[buf_size + 1];
-	int ret = 0;
 	static char *save = NULL;
 
 	if (!line)
