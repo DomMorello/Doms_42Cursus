20427de4eb4dc9cc892a7a6af5baed715d1ce9e5
DomMorello <zuzudnf@gmail.com>
2020-07-26 20:50:38 +0900

    star print practive

--- libasm/main.c
+++ libasm/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+void start(void);
 
 int main()
 {
-    
+    start();
     return 0;
 }
--- libasm/star.s
+++ libasm/star.s
@@ -6,4 +6,25 @@ start:
     mov r9, 0
 line:
     mov byte [rdx], '*'
+    inc rdx
+    inc r9
+    cmp r9, r8
+    jne line
+lineDone:
+    mov byte [rdx], 10
+    inc rdx
+    inc r8
+    mov r9, 0
+    cmp r8, maxline
+    jng line
+done:
+    mov rax, 0x02000004
+    mov rdi, 1
+    mov rsi, output
+    mov rdx, dataSize
+    syscall
     
+section .bss
+    maxline equ 8
+    dataSize equ 44
+    output: resb dataSize
