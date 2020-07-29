31dd07cea3a152ac8e131614d4430f7f4e4e48cf
Donghyeon Lee <zuzudnf@gmail.com>
2020-07-29 16:44:11 +0900

    nn

--- libasm/hello.s
+++ libasm/hello.s
@@ -11,4 +11,4 @@ start:    mov       rax, 0x02000004         ; system call for write
           syscall                           ; invoke operating system to exit
 
           section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end
+message:  db        "Hello, World", 10      ; note the newline at the end 
