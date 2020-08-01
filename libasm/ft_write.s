b9f3c6d8756a0c9cb4d74d44a36d337722ea6467
Donghyeon Lee <zuzudnf@gmail.com>
2020-08-01 20:53:43 +0900

    what

--- libasm/ft_write.s
+++ libasm/ft_write.s
@@ -1,8 +1,9 @@
-    global _ft_writes
+    global _ft_write
     section .text
 
-_ft_writes:  mov     rax, 0x02000004
-            push    rdx
-            push    rsi
-            push    rdi
+_ft_write:  mov     rax, 0x02000004
             syscall
+            jc      error
+
+error:      mov     rax, -1
+            ret
--- libasm/main.c
+++ libasm/main.c
@@ -5,7 +5,9 @@ ssize_t ft_write(int fd, const void *buf, size_t count);
 
 int main(void)
 {
-	printf("ret:%ld\n", write(1, "hello", 4));
+	int a = ft_write(1, "hello", 5);
+	printf("ret:%d\n", a);
+	printf("hello!!!!\n");
 	// int a = write(1, "hello", 24664);
 	// printf("ret:%d\n", a);
 	return 0;
