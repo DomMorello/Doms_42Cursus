aece7c77d43cc7693ca56424d61c972a6b44ac50
DomMorello <zuzudnf@gmail.com>
2020-08-01 20:45:16 +0900

    asm

--- libasm/main.c
+++ libasm/main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
-ssize_t ft_write(int fd, const void *buf, size_t count);
+// ssize_t ft_write(int fd, const void *buf, size_t count);
 
 int main(void)
 {
-	printf("ret:%ld\n", write(1, "hello", 4));
-	// int a = write(1, "hello", 24664);
-	// printf("ret:%d\n", a);
+    write(1, "hello", 5);
 	return 0;
 }
