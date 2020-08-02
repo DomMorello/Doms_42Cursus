d4b1cf3bfac0916f982f7fa88430fb75c196b46f
DomMorello <zuzudnf@gmail.com>
2020-08-02 18:19:00 +0900

    write test

--- libasm/main.c
+++ libasm/main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
 
 // ssize_t ft_write(int fd, const void *buf, size_t count);
 
 int main(void)
 {
-    write(1, "hello", 5);
+    int fd = 0;
+
+    if ((fd = open("./test.txt", O_RDWR)) < 0)
+    {    
+        printf("error!!!\n");
+        return 0;
+    }
+    write(fd, "hello", 5);
+    close(fd);
 	return 0;
 }
