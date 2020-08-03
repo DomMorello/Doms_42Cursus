f2bc3c9af639e97f2a81146e9186a011400adb86
Donghyeon Lee <zuzudnf@gmail.com>
2020-08-03 21:20:21 +0900

    ft_write done

--- libasm/main.c
+++ libasm/main.c
@@ -2,28 +2,16 @@
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <string.h>
 
 ssize_t ft_write(int fd, const void *buf, size_t count);
+//size_t ft_strlen(const char *s);
 
 int main(void)
 {
-    //int fd = 0;
-
-    //if ((fd = open("./test.txt", O_RDWR)) < 0)
-    //{    
-    //    printf("error!!! errno: %d\n", errno);
-    //    return 0;
-    // }
-    //int ret = 0;
-    //if ((ret = ft_write(fd, "what!", 5)) == -1)
-    //{
-    //    printf("write error errno: %d\n", errno);
-    //    return 0;
-    //}
-    //close(fd);
-	int ret = write(1, "hello", 123456789);
-	printf("ret sys:%d\n", ret);
-	int ret2 = ft_write(1, "hello", 123456789);
-	printf("ret ft:%d\n", ret2);
+	//char *s = "hello";
+	//printf("%lu\n", ft_strlen(s));
+	int ret = ft_write(1, "hello", 5);
+	printf("ret:%d\n", ret);
 	return 0;
 }
