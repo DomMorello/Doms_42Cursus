22905fd5f8a04600b3d426cae53fefd1fcbde391
Donghyeon Lee <zuzudnf@gmail.com>
2020-08-04 19:42:34 +0900

    strlen fail

--- libasm/ft_strlen.s
+++ libasm/ft_strlen.s
@@ -1,4 +1,15 @@
 	global _ft_strlen
 	section .text
 
-_ft_strlen:	
+_ft_strlen:	xor	rax, rax
+			mov	rsi, rdi
+			push rsi
+			jmp	getlen
+
+getlen:		mov	al, BYTE [rsi]
+			cmp al, 0x0
+			inc	rsi
+			jne	getlen
+			pop rsi
+			ret
+			
--- libasm/main.c
+++ libasm/main.c
@@ -5,12 +5,12 @@
 #include <string.h>
 
 //ssize_t ft_write(int fd, const void *buf, size_t count);
-//size_t ft_strlen(const char *s);
+size_t ft_strlen(const char *s);
 
 int main(void)
 {
 	char *s = "hello";
-	printf("%lu\n", strlen(s));
+	printf("%lu\n", ft_strlen(s));
 	//int ret = ft_write(1, "hello", 5);
 	//printf("ret:%d\n", ret);
 	
