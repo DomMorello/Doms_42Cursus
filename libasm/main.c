3444a4589158ea1ab6f5c2ea449a003b13efb9bd
Donghyeon Lee <zuzudnf@gmail.com>
2020-08-04 19:08:00 +0900

    ft_strlen

--- libasm/main.c
+++ libasm/main.c
@@ -4,14 +4,15 @@
 #include <fcntl.h>
 #include <string.h>
 
-ssize_t ft_write(int fd, const void *buf, size_t count);
+//ssize_t ft_write(int fd, const void *buf, size_t count);
 //size_t ft_strlen(const char *s);
 
 int main(void)
 {
-	//char *s = "hello";
-	//printf("%lu\n", ft_strlen(s));
-	int ret = ft_write(1, "hello", 5);
-	printf("ret:%d\n", ret);
+	char *s = "hello";
+	printf("%lu\n", strlen(s));
+	//int ret = ft_write(1, "hello", 5);
+	//printf("ret:%d\n", ret);
+	
 	return 0;
 }
