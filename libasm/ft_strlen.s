2bb7111649d4e83b73473974441ac8a5fd256b20
Donghyeon Lee <zuzudnf@gmail.com>
2020-08-04 20:34:16 +0900

    ft_strlen done

--- libasm/ft_strlen.s
+++ libasm/ft_strlen.s
@@ -1,15 +1,11 @@
 	global _ft_strlen
 	section .text
 
-_ft_strlen:	xor	rax, rax
-			mov	rsi, rdi
-			push rsi
-			jmp	getlen
+_ft_strlen:	xor rax, rax				;initializes rax to 0
 
-getlen:		mov	al, BYTE [rsi]
-			cmp al, 0x0
-			inc	rsi
-			jne	getlen
-			pop rsi
-			ret
-			
+getlen:		cmp byte [rdi + rax], 0x0	;compares a byte in rdi(first parameter:const char *s) with null(0x0)
+			je  done					;if it counters null, jump to done(return rax)
+			inc rax						;if not, increase rax: len++
+			jmp getlen					;and check again till the pointer reaches null
+
+done:		ret							;return rax
--- libasm/main.c
+++ libasm/main.c
@@ -9,8 +9,9 @@ size_t ft_strlen(const char *s);
 
 int main(void)
 {
-	char *s = "hello";
+	char *s = "faskjdhflahsdflahdslfjhasjkdfbahsbfhlesbasdjklbfasefluiabsldjfbalshjkdbf";
 	printf("%lu\n", ft_strlen(s));
+	printf("%lu\n", strlen(s));
 	//int ret = ft_write(1, "hello", 5);
 	//printf("ret:%d\n", ret);
 	
