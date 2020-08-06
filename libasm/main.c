770a0a44d8cb8fedc1b2c8dc2c6b70b43f28615a
DomMorello <zuzudnf@gmail.com>
2020-08-06 17:59:44 +0900

    ft_read gogo

--- libasm/main.c
+++ libasm/main.c
@@ -8,10 +8,27 @@
 // size_t ft_strlen(const char *s);
 // char *ft_strcpy(char *dest, const char *src);
 //int ft_strcmp(const char *s1, const char *s2);
-ssize_t read(int fd, void *buf, size_t count);
+ssize_t ft_read(int fd, void *buf, size_t count);
 
 int main(void)
 {
-	
+	int fd = 0;
+    if ((fd = open("./test.txt", O_RDONLY)) == -1)
+    {
+        printf("open error! %d\n", errno);
+        close(fd);
+        return 0;
+    }
+
+    char buf[10] = {0,};
+    int ret = 1;
+
+    while ((ret = read(fd, buf, sizeof(buf))) > 0)
+    {
+        if (errno > 0)
+            printf("error! %d\n", errno);
+        printf("%s\n", buf);
+    }
+    close(fd);
 	return 0;
 }
