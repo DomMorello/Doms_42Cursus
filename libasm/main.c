a8d228b6ce53e88273498cafe8c9a9a420b79ded
Donghyeon Lee <donglee@c1r4s2.42seoul.kr>
2020-08-09 19:02:15 +0900

    done

--- libasm/main.c
+++ libasm/main.c
@@ -185,37 +185,11 @@ int     test_write(void)
 
 int     main(void)
 {
-    //test_strlen();
-    //test_strcpy();
+    test_strlen();
+    test_strcpy();
     test_strcmp();
-    //test_strdup();
-    //test_read();
-    //test_write();
+    test_strdup();
+    test_read();
+    test_write();
     return(0);
 }
-
-// int     nothing(void)
-// {
-//     char *a = "bonjour les petits enfants";
-//     char *b = "";
-//     // char *c = NULL;
-//     char *d = "hey";
-
-
-// 	//strcmp
-// 	printf("strcmp:		%d\nft_strcmp:	%d\n__\n",strcmp("hey",""),ft_strcmp("hey",""));
-// 	printf("strcmp:		%d\nft_strcmp:	%d\n__\n",strcmp("c?lestin  le peetit coquin","c?lestin  le petit coquin"),ft_strcmp("c?lestin  le peetit coquin","c?lestin  le petit coquin"));
-// 	printf("strcmp:		%d\nft_strcmp:	%d\n__\n",strcmp("c?lestin  le petit coquin","c?lestin  le petit coquin"),ft_strcmp("c?lestin  le petit coquin","c?lestin  le petit coquin"));
-// 	printf("strcmp:		%d\nft_strcmp:	%d\n__\n",strcmp("ho","ho"),ft_strcmp("ho","ho"));
-// 	printf("strcmp:		%d\nft_strcmp:	%d\n__\n",strcmp("",""),ft_strcmp("",""));
-// 	printf("strcmp:		%d\nft_strcmp:	%d\n__\n",strcmp("hey","ho"),ft_strcmp("hey","ho"));
-// 	printf("strcmp:		%d\nft_strcmp:	%d\n__\n",strcmp("c?lestin  le ptit coquin","c?lestin  le petit coquin"),ft_strcmp("c?lestin  le ptit coquin","c?lestin  le petit coquin"));
-// 	printf("strcmp:		%d\nft_strcmp:	%d\n__\n",ft_strcmp(NULL, NULL),ft_strcmp(NULL, NULL));
-
-// 	//strdup
-// 	printf("strdup:		%s\nft_strdup:	%s\n__\n", strdup(a), ft_strdup(a));
-// 	printf("strdup:		%s\nft_strdup:	%s\n__\n", strdup(b), ft_strdup(b));
-// 	//printf("strdup:		%s\n__\nft_strdup:	%s\n", strdup(a), ft_strdup(a));
-// 	printf("strdup:		%s\nft_strdup:	%s\n__\n", strdup(d), ft_strdup(d));
-//     return(0);
-// }
