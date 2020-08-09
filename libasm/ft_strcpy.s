bc90f705d37b1f3779703ebecdfd0396264a2567
Donghyeon Lee <donglee@c1r4s2.42seoul.kr>
2020-08-09 17:52:56 +0900

    ft_strcpy fix error

--- libasm/ft_strcpy.s
+++ libasm/ft_strcpy.s
@@ -6,10 +6,12 @@ _ft_strcpy: mov rax, rdi                ;copies rdi to rax
 
 loop:       cmp byte [rsi + rcx], 0x0   ;checks if the src string reaches null
             je  done                    ;if it reaches to null jump to done
-            mov r8, [rsi + rcx]         ;r8 is tmp. stores src string's character
-            mov [rax + rcx], r8         ;and put it to dest string which is rax
+            mov dl, byte [rsi + rcx]    ;r8 is tmp. stores src string's character
+										;(error): it was 'mov r8, [rsi + rcx]' but it seemed wrong. 
+										;it must be fit when it comes to size(must add 'byte', be stored in dl(1 byte))
+            mov byte [rax + rcx], dl    ;and put it to dest string which is rax
             inc rcx                     ;increases counter : i++
             jmp loop                    ;repeat till the src string reaches null
         
 done:       mov byte [rax + rcx], 0x0   ;add null at the end of the dest string(rax)
-            ret                         ;return dest
+			ret                         ;return dest
