da4f4240f2c0a3b0b75bbddd0a2f2ada82a90e47
DomMorello <zuzudnf@gmail.com>
2020-08-10 19:09:38 +0900

    add comment

--- libasm/ft_write.s
+++ libasm/ft_write.s
@@ -1,14 +1,14 @@
-; **************************************************************************** ;
-;                                                                              ;
-;                                                         :::      ::::::::    ;
-;    ft_write.s                                         :+:      :+:    :+:    ;
-;                                                     +:+ +:+         +:+      ;
-;    By: donglee <donglee@student.42seoul.k>        +;+  +:+       +;+         ;
-;                                                 +;+;+;+;+;+   +;+            ;
-;    Created: 2020/08/09 18:59:42 by donglee           ;+;    ;+;              ;
-;    Updated: 2020/08/09 18:59:42 by donglee          ;;;   ;;;;;;;;.fr        ;
-;                                                                              ;
-; **************************************************************************** ;
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_write.s                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: marvin <marvin@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/08/09 18:59:42 by donglee           #+#    #+#              #
+#    Updated: 2020/08/10 19:09:13 by marvin           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
 
     global	_ft_write
 	extern	___error
@@ -19,10 +19,12 @@ _ft_write:  mov     rax, 0x02000004	;write system call number
             jc		error			;in FreeBSD, if syscall has an error, carry flag sets to 1
 			ret
 
-error:		push	rbx
-			mov		rbx, rax
-			call	___error
-			mov		[rax], rbx
-			mov		rax, -1			;rax has return value. -1 in error
-			pop		rbx
-			ret
+error:		push	rbx             ;to call extern function, you must align stack as much as 16byte
+                                    ;'push rbx' will make 8 bytes in stack and 'call ___error' make another 8 byte
+            mov		rbx, rax        ;if syscall fails, rax has errno. I moved it to tmp register(rbx) to use it later
+			call	___error        ;this function returns (stored in rax)the address of errno(int *) which is the location of the errno
+			mov		[rax], rbx      ;since rax is address of the errno, and rbx is errno itself, 
+                                    ;put rbx(errno(integer)) to the address of errno([rax](location of errno)).
+			mov		rax, -1			;put -1 to rax
+			pop		rbx             ;clean stack
+			ret                     ;return rax which is -1
