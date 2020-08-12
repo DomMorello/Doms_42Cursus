1fad2f7517a5e0753bd7846cfd35fbd04ee13bba
Donghyeon Lee <donglee@c1r2s2.42seoul.kr>
2020-08-12 18:40:42 +0900

    getcwd

--- minishell/test.c
+++ minishell/test.c
@@ -2,10 +2,28 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdlib.h>
+
+char *alloc(void)
+{
+	char *ret;
+
+	ret = malloc(10);
+	return ret;
+}
 
 int main(int argc, char *argv[])
 {
-	char *str = getcwd(NULL, 255);
-	printf("%s\n", str);
+//	char buf[5];
+
+	char *buf = getcwd(buf, 1);
+	char *err = strerror(errno);
+	printf("error: %s\n", err);
+	printf("%s\n", buf);
+	buf = NULL;
+	while (1)
+	{
+		;
+	}
 	return 0;
 }
