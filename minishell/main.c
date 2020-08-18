15b990fa7d39d6988838f9874a5d2ea9e069d658
DomMorello <zuzudnf@gmail.com>
2020-08-18 21:14:03 +0900

    split

--- minishell/main.c
+++ minishell/main.c
@@ -19,36 +19,129 @@ int	ft_isspace(char c)
 	return (FALSE);
 }
 
-int parse_line(char *line)
-{
-	char buf[100];
-	int i;
-	int ret;
-	struct stat file;
-	char *cwd;
+// cd 명령어 구현. 
+// int parse_line(char *line)
+// {
+// 	char buf[100];
+// 	int i;
+// 	int ret;
+// 	struct stat file;
+// 	char *cwd;
 
+// 	i = 0;
+// 	while (ft_isspace(line[i]))
+// 		i++;
+// 	if (line[i] == 'c' && line[i + 1] == 'd' && ft_isspace(line[i + 2]))
+// 	{
+// 		i += 3;
+// 		while (ft_isspace(line[i]))
+// 			i++;
+// 		if ((ret = stat(&line[i], &file)) == -1)
+// 			return -1;
+// 		if (S_ISDIR(file.st_mode))
+// 			if ((ret = chdir(&line[i])) == -1)
+// 				return -1;
+// 		cwd = getcwd(buf, sizeof(buf));
+// 		printf("cwd: %s\n", cwd);
+// 	}
+// 	else
+// 	{
+// 		printf("command not found : %s\n", &line[i]);
+// 	}
+// 	/* 어떻게 하면 space를 기준으로 명령어 토큰을 나눌 수 있을까를 생각해라 */
+// 	return 1;
+// }
+
+static int	get_each_len(char const *s, char c)
+{
+	int len;
+	int i;
+
+	len = 0;
 	i = 0;
-	while (ft_isspace(line[i]))
+	while (s[i])
+	{
+		if (s[i] != c)
+			len++;
 		i++;
-	if (line[i] == 'c' && line[i + 1] == 'd' && ft_isspace(line[i + 2]))
+	}
+	return (len);
+}
+
+static int	get_chunk_len(char const *s, char c)
+{
+	int i;
+	int len;
+
+	len = 0;
+	i = 0;
+	while (s[i])
 	{
-		i += 3;
-		while (ft_isspace(line[i]))
+		while (s[i] && s[i] == c)
 			i++;
-		if ((ret = stat(&line[i], &file)) == -1)
-			return -1;
-		if (S_ISDIR(file.st_mode))
-			if ((ret = chdir(&line[i])) == -1)
-				return -1;
-		cwd = getcwd(buf, sizeof(buf));
-		printf("cwd: %s\n", cwd);
+		if (s[i] && s[i] != c)
+		{
+			len++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
 	}
-	else
+	return (len);
+}
+
+static int	freedata(char **result, int i)
+{
+	while (i--)
+		free(result[i]);
+	free(result);
+	return (0);
+}
+
+static int	alloc_arr(char const *s, char c, char ***result)
+{
+	int chunk;
+	int each;
+	int i;
+
+	chunk = get_chunk_len(s, c);
+	if ((*result = (char **)malloc(sizeof(char *) * chunk + 1)) == 0)
+		return (0);
+	(*result)[chunk] = 0;
+	each = get_each_len(s, c);
+	i = 0;
+	while (i < chunk)
 	{
-		printf("command not found : %s\n", &line[i]);
+		if (((*result)[i] = (char *)malloc(sizeof(char) * each + 1)) == 0)
+			return (freedata((*result), i));
+		i++;
 	}
-	/* 어떻게 하면 space를 기준으로 명령어 토큰을 나눌 수 있을까를 생각해라 */
-	return 1;
+	return (1);
+}
+
+char		**ft_split(char const *s, char c)
+{
+	char	**result;
+	int		i;
+	int		j;
+	int		k;
+
+	k = 0;
+	if (alloc_arr(s, c, &result) == 0)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i] && s[i] != c)
+		{
+			j = 0;
+			while (s[i] && s[i] != c)
+				result[k][j++] = s[i++];
+			result[k++][j] = 0;
+		}
+	}
+	return (result);
 }
 
 int main(int argc, char *argv[], char *env[])
@@ -56,17 +149,26 @@ int main(int argc, char *argv[], char *env[])
 	char buf[100];
 	int ret;
 	char *line;
+	char **commands;
+	int i = 0;
 
-	// ret = 0;
-	// while (1)
-	// {
-	// 	ft_putstr_fd("\033[31mbash\033[0m\033[32m-\033[0m\033[33m3.2\033[0m$ ", 1);
-	// 	if ((ret = get_next_line(0, &line)) < 0)
-	// 	{
-	// 		printf("gnl error\n");
-	// 		exit(0);	
-	// 	}
-	// 	parse_line(line);
-	// }
+	ret = 0;
+	while (1)
+	{
+		ft_putstr_fd("\033[31mbash\033[0m\033[32m-\033[0m\033[33m3.2\033[0m$ ", 1);
+		if ((ret = get_next_line(0, &line)) < 0)
+		{
+			printf("gnl error\n");
+			exit(0);	
+		}
+		// parse_line(line);
+		commands = ft_split(line, ';');
+		i = 0;
+		while (commands[i])
+		{
+			printf("%s\n", commands[i]);
+			i++;
+		}
+	}
 	return 0;
 }
--- minishell/test.c
+++ minishell/test.c
@@ -4,9 +4,7 @@
 #include <errno.h>
 #include <stdlib.h>
 
-extern char **environ;
-
-int	main(int argc, char *argv[])
+int	main(int argc, char *argv[], char *env[])
 {
 	char	**new_argv;
 	char	*command;
@@ -15,16 +13,10 @@ int	main(int argc, char *argv[])
 	int	idx;
 	
 	command = strdup(argv[1]);
-
-	// printf("command = %s\n", command);
-
 	absolute_path = strdup("/bin/");
 	
 	// 입력받은 명렁어와 절대경로의 문자열을 합침
 	absolute_path = strcat(absolute_path, command);
-
-	// printf("absolute_path = %s\n", absolute_path);
-	
 	new_argv = (char **)malloc(sizeof(char *) * (argc + 1));
 	
 	// 명령어와 이후의 데이터를 이중 포인터에 저장
@@ -36,20 +28,13 @@ int	main(int argc, char *argv[])
 	}
 	new_argv[argc] = NULL;
 	
-	// new_argv에 잘 들어갔나 확인
-	// idx = 0;
-	// while(new_argv[idx])
-	// {
-	// 	printf("%s\n", new_argv[idx]);
-	// 	idx++;
-	// }
-	
 	// 명령어 실행!
-	if (execve(absolute_path, new_argv, environ) == -1)
+	if (execve(absolute_path, new_argv, env) == -1)
 	{
 		fprintf(stderr, "error: %s\n", strerror(errno));
 		return (1);
 	}
 	printf("this is not exec\n");
+
 	return (0);
 }
