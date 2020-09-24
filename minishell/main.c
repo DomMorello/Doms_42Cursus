5a78e66044fde5799ceb19ee57351543a856c73d
DomMorello <zuzudnf@gmail.com>
2020-09-24 11:03:24 +0900

    pipe test

--- minishell/main.c
+++ minishell/main.c
@@ -49,6 +49,75 @@ void set_red(char *cmd[])
         {
             /* 이런 구조로 한 번 짜봐서 함수화가 어떻게 진행될지를 짜보자. */
         }
+        
+        i++;
+    }
+}
+
+void pipeline(char** cmd) 
+{ 
+    // 0 is read end, 1 is write end 
+    int fd[2];  
+    pid_t p1, p2; 
+  
+    if (pipe(fd) < 0) { 
+        printf("\nPipe could not be initialized"); 
+        return; 
+    } 
+    p1 = fork(); 
+    if (p1 < 0) { 
+        printf("\nCould not fork"); 
+        return; 
+    } 
+  
+    if (p1 == 0) { 
+        // Child 1 executing.. 
+        // It only needs to write at the write end 
+        close(fd[0]); 
+        dup2(fd[1], 1); 
+        close(fd[1]); 
+  
+        if (execlp("ls", "ls", "-al", NULL) < 0) { 
+            printf("\nCould not execute command 1.."); 
+            exit(0); 
+        } 
+    } else { 
+        // Parent executing 
+        p2 = fork(); 
+  
+        if (p2 < 0) { 
+            printf("\nCould not fork"); 
+            return; 
+        } 
+  
+        // Child 2 executing.. 
+        // It only needs to read at the read end 
+        if (p2 == 0) { 
+            close(fd[1]); 
+            dup2(fd[0], 0); 
+            close(fd[0]); 
+            if (execlp("grep", "grep", "h", NULL) < 0) { 
+                printf("\nCould not execute command 2.."); 
+                exit(0); 
+            } 
+        } else { 
+            // parent executing, waiting for two children 
+            wait(NULL); 
+        } 
+    } 
+}
+
+void set_pipe(char *cmd[])
+{
+    int i;
+
+    i = 0;
+    while (cmd[i])
+    {
+        if (!strcmp(cmd[i], "|"))
+        {
+            pipeline(cmd);
+        }
         i++;
     }
 }
@@ -59,10 +128,10 @@ void test(void)
 {
     pid_t pid;
     int status;
-    char *command[8] = {"ls", "-al", "|", "grep", "D", ">", "test", NULL};
+    char *command[8] = {"ls", "-al", "|", "grep", "h", ">", "test", NULL};
 
     set_red(command);
-    // set_pipe(command);
+    set_pipe(command);
     // exex_cmd(command);
 }
 
