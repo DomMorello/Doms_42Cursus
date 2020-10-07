260d7a30ce8a1af263fa5015c7fde10f81dc68c3
DomMorello <zuzudnf@gmail.com>
2020-10-07 14:20:31 +0900

    ??

--- minishell/main.c
+++ minishell/main.c
@@ -6,28 +6,6 @@ int g_pipe_fd[2];
 int g_red_out_fd;
 int g_red_in_fd;
 
-// void set_red_out(char *cmd[], int red_idx)
-// {
-//     g_red_out_fd = open(cmd[red_idx + 1], O_CREAT | O_RDWR);
-//     /* 이렇게 하면 > 가 여러개 나오더라도 마지막에 나온 fd가 복제되니까 괜찮겠찌? */
-//     dup2(g_red_out_fd, 1);
-//     close(g_red_out_fd);
-// }
-
-void set_prev_pipe()
-{
-	close(g_pipe_fd[1]);
-	dup2(g_pipe_fd[0], 0);
-	close(g_pipe_fd[0]);
-}
-
-void set_post_pipe()
-{
-	close(g_pipe_fd[0]);
-	dup2(g_pipe_fd[1], 1);
-	close(g_pipe_fd[1]);
-}
-
 void set_red_out(char *title)
 {
 	g_red_out_fd = open(title, O_CREAT | O_RDWR);
@@ -35,7 +13,7 @@ void set_red_out(char *title)
 	close(g_red_out_fd);
 }
 
-void set_pipe(char *cmd[], int pipe_idx)
+void set_pipe_child(char *cmd[], int pipe_idx)
 {
     pid_t pid;
     static int i = 0;
@@ -49,33 +27,52 @@ void set_pipe(char *cmd[], int pipe_idx)
         close(g_pipe_fd[0]);
         dup2(g_pipe_fd[1], 1);
         close(g_pipe_fd[1]);
-        if (i == 1)
-            execlp("ls", "ls", "-al", NULL);
-        if (i == 2)
-            execlp("grep", "grep", "Sep", NULL);
+        execlp("ls", "ls", "-al", NULL);
+       
     }
+    // if (i == 2)
+    // {
+    //     char buf[1000];
+    //     read(g_pipe_fd[0], buf, sizeof(buf));
+    //     perror("read");
+    //     printf("%s\n", buf);
+    // }
+}
+
+void set_pipe_parent(char *cmd[], int pipe_idx)
+{
     close(g_pipe_fd[1]);
     dup2(g_pipe_fd[0], 0);
     close(g_pipe_fd[0]);
+    execlp("grep", "grep", "Sep", NULL);
 }
 
 void test(void)
 {
     /* ;콜론으로 나눠진 것이 여기로 들어왔다고 가정하자! */
     char *cmd[6] = {"ls", "-al", "|", "grep", "Sep", NULL};
-    /* 위에 짠 걸로 이렇게만이라도 되나 한 번 해보자 */
+    
     int i;
+    int is_pipe;
 
     i = 0;
+    is_pipe = 0;
     while (cmd[i])
     {
         if (!strcmp(cmd[i], "|"))
-            set_pipe(cmd, i);
-        if (!strcmp(cmd[i], ">"))
-            set_red_out(cmd[i + 1]);
+        {
+            set_pipe_child(cmd, i);
+            is_pipe = 1;
+        }
+        if (is_pipe)
+        {
+            set_pipe_parent(cmd, i);
+            is_pipe = 0;
+        }
+        // if (!strcmp(cmd[i], ">"))
+        //     set_red_out(cmd[i + 1]);
         i++;
     }
-    
 }
 
 int main(int argc, char *argv[])
