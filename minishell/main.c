c0e5d16ff1a7b2c31288617cc4ea0754bff7359b
DomMorello <zuzudnf@gmail.com>
2020-10-19 17:20:09 +0900

    pivotal login done

--- minishell/main.c
+++ minishell/main.c
@@ -17,6 +17,7 @@ void set_red_out(char *title)
 void set_red_in(char *title)
 {
 	g_red_in_fd = open(title, O_CREAT | O_RDWR);
+    perror("red in open");
 	dup2(g_red_in_fd, 0);
 	close(g_red_in_fd);
 }
@@ -66,20 +67,24 @@ void exec_cmd(char *cmd[], int *prev_pipe_idx, int pipe_idx)
 
     i++;
     pid_t pid = fork();
-    wait(NULL);
     if (pid == 0)
     {
         set_pipe_child();
         process_redirection(cmd, prev_pipe_idx, pipe_idx);
         if (i == 1)
-            execlp("ls", "ls", "-al", NULL);
+            execlp("grep", "grep", "Sep", NULL);
         if (i == 2)
             execlp("grep", "grep", "Sep", NULL);
         if (i == 3)
             execlp("wc", "wc", NULL);
     }
+    else if (pid < 0)
+    {
+        perror("fork err");
+    }
     else
     {
+        wait(NULL);
         set_pipe_parent();
     }
 }
@@ -89,6 +94,7 @@ void process_pipe(char *cmd[], int pipe_idx, int *prev_pipe_idx)
     if (!strcmp(cmd[pipe_idx], "|"))
     {
         pipe(g_pipe_fd);
+        perror("pipe err");
         exec_cmd(cmd, prev_pipe_idx, pipe_idx);
         *prev_pipe_idx = pipe_idx;
     }
@@ -97,19 +103,20 @@ void process_pipe(char *cmd[], int pipe_idx, int *prev_pipe_idx)
 void exec_last_cmd(char *cmd[], int *prev_pipe_idx, int pipe_idx)
 {
     process_redirection(cmd, prev_pipe_idx, pipe_idx);
-    execlp("echo", "hi", NULL);
+    execlp("wc", "wc", NULL);
 }
 
 void test(void)
 {
     /* ;콜론으로 나눠진 것이 여기로 들어왔다고 가정하자! */
-    char *cmd[50] = {"ls", "-al", "|", "grep", "Sep", "|", "wc", ">",
-            "hello1", ">", "hello2", "|", "echo", "hi", ">", "hello3", NULL};
+    // char *cmd[50] = {"ls", "-al", "|", "grep", "Sep", "|", "wc", ">",
+    //         "hello1", ">", "hello2", "|", "echo", "hi", ">", "hello3", NULL};
     
     // char *cmd[50] = {"grep", "Sep", "<", "hello1", "|", "wc", "<", "hello1", NULL};
     
     // char *cmd[50] = {"ls", "-al", "|", "grep", "Sep", ">>", "hello1", "|",
     //                 "echo", "hi", ">>", "hello1", NULL};
+    char *cmd[50] = {"grep", "Sep", "<", "hello1", "|", "wc", ">>", "hello1", ">>", "hello2", NULL};
 
     int i;
     int prev_pipe_idx;
