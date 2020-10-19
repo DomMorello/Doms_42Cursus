1669e46fba7a344a756ace13159b9f09de285e67
DomMorello <zuzudnf@gmail.com>
2020-10-19 23:54:38 +0900

    readdir test

--- minishell/main.c
+++ minishell/main.c
@@ -61,6 +61,46 @@ void process_redirection(char *cmd[], int *prev_pipe_idx, int pipe_idx)
     }
 }
 
+void handle_executable(char *cmd[], int prev_pipe_idx, int pipe_idx)
+{
+    struct dirent *dir;
+    DIR *dir_ptr;
+
+    dir_ptr = opendir("/usr/bin");
+    perror("opendir err");
+    dir = readdir(dir_ptr);
+    perror("readdir err");
+    printf("!! %s\n", dir->d_name);
+    while (dir)
+    {
+        dir = readdir(dir_ptr);
+        printf("!! %s\n", dir->d_name);
+    }
+    // while (prev_pipe_idx < pipe_idx)
+    // {
+        
+    // }
+}
+
+void handle_executable2(char *cmd[], int prev_pipe_idx, int pipe_idx)
+{
+    while (++prev_pipe_idx < pipe_idx)
+    {
+        printf("handle: %s\n", cmd[prev_pipe_idx]);
+    }
+}
+
+void parse_cmd(char *cmd[], int *prev_pipe_idx, int pipe_idx)
+{
+    int i;
+
+    i = *prev_pipe_idx;
+    if (i == 0)
+        handle_executable(cmd, i, pipe_idx); //함수 이름은 나중에 적절하게 바꾸자
+    else
+        handle_executable2(cmd, i, pipe_idx);
+}
+
 void exec_cmd(char *cmd[], int *prev_pipe_idx, int pipe_idx)
 {
     static int i = 0;
@@ -69,14 +109,16 @@ void exec_cmd(char *cmd[], int *prev_pipe_idx, int pipe_idx)
     pid_t pid = fork();
     if (pid == 0)
     {
-        set_pipe_child();
-        process_redirection(cmd, prev_pipe_idx, pipe_idx);
-        if (i == 1)
-            execlp("grep", "grep", "Sep", NULL);
-        if (i == 2)
-            execlp("grep", "grep", "Sep", NULL);
-        if (i == 3)
-            execlp("wc", "wc", NULL);
+        // set_pipe_child();
+        // process_redirection(cmd, prev_pipe_idx, pipe_idx);
+        // if (i == 1)
+        //     execlp("grep", "grep", "Sep", NULL);
+        // if (i == 2)
+        //     execlp("grep", "grep", "Sep", NULL);
+        // if (i == 3)
+        //     execlp("wc", "wc", NULL);
+        parse_cmd(cmd, prev_pipe_idx, pipe_idx);
+        exit(1);
     }
     else if (pid < 0)
     {
@@ -85,16 +127,16 @@ void exec_cmd(char *cmd[], int *prev_pipe_idx, int pipe_idx)
     else
     {
         wait(NULL);
-        set_pipe_parent();
+        // set_pipe_parent();
     }
 }
 
-void process_pipe(char *cmd[], int pipe_idx, int *prev_pipe_idx)
+void process_pipe(char *cmd[], int *prev_pipe_idx, int pipe_idx)
 {
     if (!strcmp(cmd[pipe_idx], "|"))
     {
-        pipe(g_pipe_fd);
-        perror("pipe err");
+        // pipe(g_pipe_fd);
+        // perror("pipe err");
         exec_cmd(cmd, prev_pipe_idx, pipe_idx);
         *prev_pipe_idx = pipe_idx;
     }
@@ -102,21 +144,23 @@ void process_pipe(char *cmd[], int pipe_idx, int *prev_pipe_idx)
 
 void exec_last_cmd(char *cmd[], int *prev_pipe_idx, int pipe_idx)
 {
-    process_redirection(cmd, prev_pipe_idx, pipe_idx);
-    execlp("wc", "wc", NULL);
+    // process_redirection(cmd, prev_pipe_idx, pipe_idx);
+    // execlp("wc", "wc", NULL);
+    parse_cmd(cmd, prev_pipe_idx, pipe_idx);
 }
 
 void test(void)
 {
     /* ;콜론으로 나눠진 것이 여기로 들어왔다고 가정하자! */
-    // char *cmd[50] = {"ls", "-al", "|", "grep", "Sep", "|", "wc", ">",
-    //         "hello1", ">", "hello2", "|", "echo", "hi", ">", "hello3", NULL};
-    
+    char *cmd[50] = {"ls", "-al", "|", "grep", "Sep", "|", "wc", ">",
+            "hello1", ">", "hello2", "|", "echo", "hi", ">", "hello3", NULL};
+
     // char *cmd[50] = {"grep", "Sep", "<", "hello1", "|", "wc", "<", "hello1", NULL};
-    
+
     // char *cmd[50] = {"ls", "-al", "|", "grep", "Sep", ">>", "hello1", "|",
     //                 "echo", "hi", ">>", "hello1", NULL};
-    char *cmd[50] = {"grep", "Sep", "<", "hello1", "|", "wc", ">>", "hello1", ">>", "hello2", NULL};
+
+    // char *cmd[50] = {"grep", "Sep", "<", "hello1", "|", "wc", ">>", "hello1", ">>", "hello2", NULL};
 
     int i;
     int prev_pipe_idx;
@@ -125,7 +169,7 @@ void test(void)
     i = 0;
     while (cmd[i])
     {
-        process_pipe(cmd, i, &prev_pipe_idx);
+        process_pipe(cmd, &prev_pipe_idx, i);
         i++;
         if (!cmd[i])
             exec_last_cmd(cmd, &prev_pipe_idx, i);
