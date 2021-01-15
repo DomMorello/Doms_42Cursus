5670f3263698caffdd2a6957ed8abec28f835eff
DomMorello <zuzudnf@gmail.com>
2021-01-15 23:02:41 +0900

    metallb config doing: bug:: LoadBalancer not working

---
+++ ft_services/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "metalLB manifest"
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+cd ./srcs/metalLB
