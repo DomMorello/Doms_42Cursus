ef3d414599493940c0a128149d5acb09a6d62efc
DomMorello <zuzudnf@gmail.com>
2021-01-27 22:45:14 +0900

    dashboard activate done

---
+++ ft_services/note.md
@@ -0,0 +1,3 @@
+웹 UI 대시보드 설정 방법
+(https://fliedcat.tistory.com/171)<br>
+
--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-echo "metalLB manifest"
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+# echo "setting k8s dashboard"
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+# kubectl proxy
 
-cd ./srcs/metalLB
+# token setting
+# cd srcs; cd dashboard;
+# kubectl apply -f dashboard-adminuser.yaml
+# kubectl apply -f rollbinding.yaml
+# kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+# copy and paste token part and do login on the browser(below address)
+# http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
+
+echo "metalLB manifest"
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+# cd ./srcs/metalLB
