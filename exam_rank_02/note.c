9688f48beeaa9ff69147b840825bb884dc0ed7ad
DomMorello <zuzudnf@gmail.com>
2021-02-20 00:00:07 +0900

    proto

---
+++ exam_rank_02/note.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int ft_strlen(char *s);
+char *ft_strchr(char *s, int c);
+char *ft_substr(char *s, int start, int len);
+void ft_strdel(char **as);
+int ft_strlcpy(char *dst, char *src, int dstsize);
+char *ft_strdup(char *s);
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
