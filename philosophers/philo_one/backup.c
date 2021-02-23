90b80ccdd408756f42626b0a9fbba22d3c6bb235
Donghyeon Lee <zuzudnf@gmail.com>
2021-02-23 18:01:15 +0900

    header

---
+++ philosophers/philo_one/backup.c
@@ -0,0 +1,24 @@
+#include "./philo_one.h"
+
+# define N 5
+
+int main(int argc, char const *argv[])
+{
+
+    if (argc < 5 || argc > 6)
+    {
+        printf("invalid argument\n");
+        return (ERROR);
+    }
+    
+	// init structure(arg, philo)
+    // 5개 스레드(철학자)를 만들고
+    // 5개 포크(뮤텍스)를 만든다
+    // 각자의 스레드는 같은 함수를 실행한다
+    // 포크를 쥘 때 좌우가 먹고 있는지를 검사한다
+    // 좌우가 안 먹고 있다면 먹기 시작한다
+    // 해당 포크에 대해서만 뮤텍스를 락한다
+    // 그러면 인접한 철학자는 대기하고 있겠지
+
+    return 0;
+}
--- philosophers/philo_one/philo_one.h
+++ philosophers/philo_one/philo_one.h
@@ -8,23 +8,35 @@
 #include <pthread.h>
 #include <string.h>
 
+# define TRUE 1
+# define FALSE 0
+# define ERROR -1
 
 typedef struct		s_arg
 {
-    int             num_of_philo;	//equals to num_of_forks
-    unsigned long	die_time;		//thread 에서 die 되면 바로 프로그램 종료되게끔
-    unsigned long	eat_time; 		//이 시간동안 usleep?
-    unsigned long	sleep_time;
-    int				must_eat;		//이렇게 다 먹으면 종료
+	int				num_of_philos;
+	unsigned long	time_to_die;
+	unsigned long	time_to_eat;
+	unsigned long	time_to_sleep;
+	int				must_eat_cnt;
 }					t_arg;
 
 typedef struct 		s_philo
 {
-	int				philo_no;
-	int				is_dead;		//필요할까?
-	t_arg			*arg;			
+	int 			philo_no;
 	int				l_fork;
 	int				r_fork;
+	int				is_eating;
+	int				is_sleeping;
+	int				is_thinking;
+	unsigned long	limit;
+	unsigned long 	last_eat;
 }					t_philo;
+
+typedef struct		s_data
+{
+	t_arg			*arg;
+	t_philo			*philo;
+}					t_data;
 
 #endif
