6afcf2fb47cb8a30c8e5004d15b940deed2a685c
Donghyeon Lee <zuzudnf@gmail.com>
2021-02-26 13:03:46 +0900

    seg fault

--- philosophers/philo_one/test.c
+++ philosophers/philo_one/test.c
@@ -1,27 +1,7 @@
 #include "./philo_one.h"
 
-typedef struct s_test
-{
-	int id;
-	pthread_mutex_t *forks;
-}				t_test;
-
-void *routine(void *arg)
-{
-}
-
 int main(int argc, char const *argv[])
 {
-	t_test philo;
 
-	int i = 0;
-
-	while (i < 3)
-	{
-		philo.forks = malloc(sizeof(pthread_mutex_t) * 3);
-		pthread_mutex_init(&philo.forks[i], NULL);
-		i++;
-	}
-	
     return 0;
 }
