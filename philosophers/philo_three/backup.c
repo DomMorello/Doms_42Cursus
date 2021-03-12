9605bca03a46e8cad576ff24f9425aac468299e9
Donghyeon Lee <zuzudnf@gmail.com>
2021-03-12 10:16:04 +0900

    init info , philos

--- philosophers/philo_three/backup.c
+++ philosophers/philo_three/backup.c
@@ -2,7 +2,50 @@
 
 int err_check(char const *argv[], t_info *info)
 {
-	if (())
+	if ((info->num_of_philos = ft_atoi(argv[1])) <= 1 ||
+		(info->time_to_die = ft_atoi(argv[2])) <= 0 ||
+		(info->time_to_eat = ft_atoi(argv[3])) <= 0 ||
+		(info->time_to_sleep = ft_atoi(argv[4])) <= 0)
+		return (TRUE);
+	if (argv[5] && (info->must_eat = ft_atoi(argv[5])) <= 0)
+		return (TRUE);
+	else if (!argv[5])
+		info->must_eat = -1;
+	return (FALSE);
+}
+
+void init_info(t_info *info)
+{
+	int i;
+
+	i = -1;
+//	while (++i < info->num_of_philos)
+//	{
+//		info->philos[i].philo_no = i + 1;
+//		info->philos[i].eat_cnt = 0;
+//	}
+	info->full_philo = 0;
+	info->done = 0;
+	info->time = get_time();
+	info->enter = sem_open(SEM_ENTER, O_CREAT | O_EXCL, 0777, info->num_of_philos / 2);
+	info->msg = sem_open(SEM_MSG, O_CREAT | O_EXCL, 0777, 1);
+	sem_unlink(SEM_ENTER);
+	sem_unlink(SEM_MSG);
+}
+
+void init_philos(t_info *info)
+{
+	int i;
+
+	i = -1;
+	while (++i < info->num_of_philos)
+	{
+		info->philos[i].philo_no = i + 1;
+		info->philos[i].eat_cnt = 0;
+		info->philos[i].info = info;
+		info->philos[i].last_eat = get_time();
+		//fork
+	}
 }
 
 int main(int argc, char const *argv[])
@@ -11,6 +54,9 @@ int main(int argc, char const *argv[])
 
 	if (!(argc == 5 || argc == 6) || err_check(argv, &info))
 		return (printf("invalid arguments\n"));
-
+	if ((info.philos = (t_philo *)malloc(sizeof(t_philo) * info.num_of_philos)) == NULL)
+		return (ERROR);
+	init_info(&info);
+	init_philos(&info);
 	return (TRUE);
 }
---
+++ philosophers/philo_three/util.c
@@ -0,0 +1,33 @@
+#include "./philo_three.h"
+
+int		ft_atoi(char const *str)
+{
+	int	i;
+	int	start;
+	int	is_neg;
+	int	res;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' ||
+			str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	is_neg = (str[i] == '-') ? -1 : 1;
+	if (is_neg == -1 || str[i] == '+')
+		i++;
+	start = i;
+	res = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+		res = (res * 10) + (str[i++] - '0');
+	return (res * is_neg);
+}
+
+unsigned long	get_time(void)
+
+{
+	struct timeval		tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
