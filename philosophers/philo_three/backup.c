255b2a0fa73078b04f9407b6d7dc0d1c3bfad8cc
Donghyeon Lee <zuzudnf@gmail.com>
2021-03-12 09:27:24 +0900

    start philo_three

---
+++ philosophers/philo_three/backup.c
@@ -0,0 +1,16 @@
+#include "./philo_three.h"
+
+int err_check(char const *argv[], t_info *info)
+{
+	if (())
+}
+
+int main(int argc, char const *argv[])
+{
+	t_info info;
+
+	if (!(argc == 5 || argc == 6) || err_check(argv, &info))
+		return (printf("invalid arguments\n"));
+
+	return (TRUE);
+}
---
+++ philosophers/philo_three/philo_three.h
@@ -0,0 +1,57 @@
+#ifndef PHILO_THREE_H
+#define PHILO_THREE_H
+
+#define TRUE 1
+#define FALSE 0
+#define ERROR -1
+
+#define SEM_ENTER "/enter"
+#define SEM_MSG "/msg"
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <semaphore.h>
+# include <signal.h>
+# include <sys/time.h>
+
+enum	e_state{
+	S_TAKEN_FORK,
+	S_EATING,
+	S_SLEEPING,
+	S_THINKING,
+	S_DIED	
+};
+
+struct s_info;
+
+typedef struct		s_philo
+{
+	int				philo_no;
+	int				eat_cnt;
+	unsigned long	last_eat;
+	pid_t			pid;
+	struct s_info	*info;
+}					t_philo;
+
+typedef struct		s_info
+{
+	int				num_of_philos;
+	unsigned long	time_to_die;
+	unsigned long	time_to_eat;
+	unsigned long	time_to_sleep;
+	int				must_eat;
+	unsigned long	time;
+	int				full_philo;
+	int				done;
+	sem_t			*enter;
+	sem_t			*msg;
+	t_philo			*philos;
+}					t_info;
+
+int ft_atoi(char const *s);
+unsigned long	get_time(void);
+
+#endif
