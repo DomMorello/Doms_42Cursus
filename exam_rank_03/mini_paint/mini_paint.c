f97cc8ff1b6f11be41296f8c8e1d1a3277238756
Donghyeon Lee <zuzudnf@gmail.com>
2021-03-16 15:51:21 +0900

    found buf and solved

--- exam_rank_03/mini_paint/mini_paint.c
+++ exam_rank_03/mini_paint/mini_paint.c
@@ -15,7 +15,7 @@ int ft_strlen(char *s)
 		len++;
 	return len;
 }
-
+//에러메세지를 출력하고 에러인 경우에는 항상 1 리턴
 int str_error(char *s)
 {
 	write(1, s, ft_strlen(s));
@@ -28,33 +28,40 @@ int main(int argc, char *argv[])
 	char *image;
 	int read;
 
+	//배경 높이, 넓이, 배경문자, 반복문 안에서 돌아가는 x,y좌표
 	int b_width;
 	int b_height;
 	char b_char;
 	int x;
 	int y;
 	
+	//원 관련 정보
 	char type;
 	float center_x;
 	float center_y;
-	float r;
+	float r;	//반지름
 	char c_char;
-	float distance;
+	float distance;	//현재좌표와 원의 중심과의 거리
 
+	// 인자 2개가 아닌 경우 에러
 	if (argc != 2)
 		return (str_error(E_ARG));
+	// fopen 실패시 에러
 	if (!(file = fopen(argv[1], "r")))
 		return (str_error(E_OP));
+	// 배경정보가 3개가 아니면 에러
 	if (fscanf(file, "%d %d %c\n", &b_width, &b_height, &b_char) != 3)
 		return (str_error(E_OP));
+	// 0 초과 300 이하가 아니면 에러처리
 	if (!(b_width > 0 && b_width <= 300 && b_height > 0 && b_height <= 300))
 		return (str_error(E_OP));
 	image = (char *)malloc(sizeof(char) * (b_width * b_height));
-	memset(image, b_char, b_width * b_height);
+	memset(image, b_char, b_width * b_height);	//일단 배경색으로 다 칠함
 	read = fscanf(file, "%c %f %f %f %c\n", &type, &center_x, &center_y, &r, &c_char);
 	while (read == 5)
 	{
-		if (r < 0 || !(type == 'c' || type == 'C'))
+		//반지름이 0 미만이거나 아 여기서 0 인 경우에 처리를 못해줬네....
+		if (r <= 0 || !(type == 'c' || type == 'C'))
 		{
 			free(image);
 			fclose(file);
@@ -82,8 +89,8 @@ int main(int argc, char *argv[])
 			y++;
 		}
 		read = fscanf(file, "%c %f %f %f %c\n", &type, &center_x, &center_y, &r, &c_char);
-	}	
-	
+	}
+	// eof를 만나기 전에 다른 값이 들어오면 에러처리
 	if (read != -1)
 	{
 		free(image);
@@ -99,7 +106,6 @@ int main(int argc, char *argv[])
 	}
 	free(image);
 	fclose(file);
-
 	return 0;
 }
 
