76338b13741156a150b8171cdcff24c6430cd38a
DomMorello <zuzudnf@gmail.com>
2021-03-29 16:56:31 +0900

    ex01 done

--- CPP_Module/CPP_module_02/ex01/Fixed.cpp
+++ CPP_Module/CPP_module_02/ex01/Fixed.cpp
@@ -5,12 +5,14 @@ Fixed::Fixed() : value(0) {
 }
 
 Fixed::Fixed(const int val) {
-    value = val << bits;    //val * 2^8(bits)
+    value = val << bits;
+    //맨 오른 쪽 8비트는 소수부분을 담아야 하기 때문에
+    //정수는 정수부분으로 왼 쪽으로 8칸 옮겨줘야 정수부분에 위치한다.
     std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed(const float val) {
-    value = roundf(val * (1 << bits));
+    value = roundf(val * (1 << bits));  //부동소수점으로 변환
     std::cout << "Float constructor called" << std::endl;
 } 
 
@@ -42,6 +44,8 @@ float   Fixed::toFloat(void) const {
 }
 
 int     Fixed::toInt(void) const {
+    //Int 생성자에서 고정소수점으로 바꾼 것을 다시 정수로 바꾸려면
+    //왼쪽으로 8번 옮겼던 것을 다시 오른쪽으로 8번 되돌려준다.
     return value >> bits;
 }
 
