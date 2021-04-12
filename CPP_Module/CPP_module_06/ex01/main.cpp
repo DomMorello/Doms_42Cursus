7dcb8b6f9769d41305664fa69f3809742b8e1769
donglee <zuzudnf@gmail.com>
2021-04-12 22:54:17 +0900

    ex01 done

--- CPP_Module/CPP_module_06/ex01/main.cpp
+++ CPP_Module/CPP_module_06/ex01/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <ctime>
-//#include <string.h>
+#include <string.h>
 
 struct Data
 {
@@ -11,44 +11,47 @@ struct Data
 };
 
 void *serialize(void) {
-    const char alphanum[] = "0123456789abcdefghijklmnopqrstuvwxyz";
-  //  std::string s1("");
-    //std::string s2("");
-   // int randInt;
-
+    std::string alphanum = "0123456789abcdefghijklmnopqrstuvwxyz";
+    char *ret = new char[20];
+    
     srand(clock());
-    randInt = rand();
+    //랜덤한 숫자 혹은 알파벳을 문자열 ret 첫 8자리에 넣는다.
     for (size_t i = 0; i < 8; i++)
-    {
-        s1 += alphanum[rand() % (strlen(alphanum) - 1)];
-        s2 += alphanum[rand() % (strlen(alphanum) - 1)];
-    }
-    char *ret = new char[sizeof(s1) + sizeof(randInt) + sizeof(s2)];
-    std::cout << "s1 : " << s1 << std::endl;
-    std::cout << "n : " << randInt << std::endl;
-    std::cout << "s2 : " << s2 << std::endl;
-
-//    memcpy(ret, &s1, sizeof(s1));
-  //  memcpy(ret + sizeof(s1), &randInt, sizeof(randInt));
-    //memcpy(ret + sizeof(s1), &s2, sizeof(s2));
-    return ret;
+        ret[i] = alphanum.at(rand() % alphanum.length());
+    std::cout << "ret first 8 bytes: " << std::string(ret, 8) << std::endl;
+    //문자열 ret + 8 위치부터 4바이트만큼 int값을 랜덤하게 넣는다.
+    *reinterpret_cast<int*>(ret + 8) = static_cast<int>(rand());
+    std::cout << "ret next 4 bytes: " << *reinterpret_cast<int*>(ret + 8) << std::endl;
+    //랜덤한 숫자 혹은 알파벳을 문자열 ret + 12 위치부터 8자리 넣는다.
+    for (size_t i = 12; i < 20; i++)
+        ret[i] = alphanum.at(rand() % alphanum.length());
+    std::cout << "ret last 8 bytes: " << std::string(ret + 12, 8) << std::endl;
+    return reinterpret_cast<void*>(ret);
 }
 
 Data* deserialize(void *raw) {
     Data *data = new Data;
-
-    data->s1 = reinterpret_cast<char*>(raw);
-    data->n = *reinterpret_cast<int*>(reinterpret_cast<char*>(raw) + sizeof(data->s1));
-    data->s2 = reinterpret_cast<char*>(raw) + sizeof(data->s1) + sizeof(data->s2);
+    //void* 형을 char*으로 형변환
+    char *tmp = reinterpret_cast<char*>(raw);
+    
+    //char* tmp 첫 8자리를 s1에 string으로 넣음
+    data->s1 = std::string(tmp, 8);
+    //int 4바이트만큼 tmp + 8 위치에 넣는다.
+    data->n = *reinterpret_cast<int*>(tmp + 8);
+    //tmp + 12 자리부터 8자리를 s2에 string으로 넣음
+    data->s2 = std::string((tmp + 12), 8);
     return data;
 }
 
 int main()
 {
-    char *serial = reinterpret_cast<char*>(serialize());
-    Data *deserial = deserialize(serial);
+    char *raw = reinterpret_cast<char*>(serialize());
+    Data *deserial = reinterpret_cast<Data*>(deserialize(raw));
 
-    std::cout << deserial->s1 << std::endl;
+    std::cout << deserial->s1 <<std::endl;
     std::cout << deserial->n << std::endl;
-    std::cout << deserial->s2 << std::endl;  
+    std::cout << deserial->s2 << std::endl;
+
+    delete raw;
+    delete deserial;
 }
