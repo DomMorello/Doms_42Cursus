b41edabf6bf54ccf62fb0ad64d28951d9aa6740d
Donghyeon Lee <donglee@c3r2s2.42seoul.kr>
2021-04-12 23:50:34 +0900

    fix to 52byte by using c++ string

--- CPP_Module/CPP_module_06/ex01/main.cpp
+++ CPP_Module/CPP_module_06/ex01/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <ctime>
-#include <string.h>
 
 struct Data
 {
@@ -12,34 +11,31 @@ struct Data
 
 void *serialize(void) {
     std::string alphanum = "0123456789abcdefghijklmnopqrstuvwxyz";
-    char *ret = new char[20];
+	std::string s1("");
+	std::string s2("");
+	int n;
+	char *ret = new char[sizeof(s1) + sizeof(int) + sizeof(s2)];
     
     srand(clock());
-    //랜덤한 숫자 혹은 알파벳을 문자열 ret 첫 8자리에 넣는다.
     for (size_t i = 0; i < 8; i++)
-        ret[i] = alphanum.at(rand() % alphanum.length());
-    std::cout << "ret first 8 bytes: " << std::string(ret, 8) << std::endl;
-    //문자열 ret + 8 위치부터 4바이트만큼 int값을 랜덤하게 넣는다.
-    *reinterpret_cast<int*>(ret + 8) = static_cast<int>(rand());
-    std::cout << "ret next 4 bytes: " << *reinterpret_cast<int*>(ret + 8) << std::endl;
-    //랜덤한 숫자 혹은 알파벳을 문자열 ret + 12 위치부터 8자리 넣는다.
-    for (size_t i = 12; i < 20; i++)
-        ret[i] = alphanum.at(rand() % alphanum.length());
-    std::cout << "ret last 8 bytes: " << std::string(ret + 12, 8) << std::endl;
+	{
+		s1 += alphanum[rand() % alphanum.length()];
+		s2 += alphanum[rand() % alphanum.length()];
+	}
+	n = rand();
+	memcpy(ret, &s1, sizeof(s1));
+	memcpy(ret + sizeof(s1), &n, sizeof(n));
+	memcpy(ret + sizeof(s1) + sizeof(n), &s2, sizeof(s2));
+
     return reinterpret_cast<void*>(ret);
 }
 
 Data* deserialize(void *raw) {
     Data *data = new Data;
-    //void* 형을 char*으로 형변환
-    char *tmp = reinterpret_cast<char*>(raw);
     
-    //char* tmp 첫 8자리를 s1에 string으로 넣음
-    data->s1 = std::string(tmp, 8);
-    //int 4바이트만큼 tmp + 8 위치에 넣는다.
-    data->n = *reinterpret_cast<int*>(tmp + 8);
-    //tmp + 12 자리부터 8자리를 s2에 string으로 넣음
-    data->s2 = std::string((tmp + 12), 8);
+	data->s1 = reinterpret_cast<char*>(raw);
+	data->n = *reinterpret_cast<int*>(raw) + sizeof(data->s1);
+	data->s2 = reinterpret_cast<char*>(raw) + sizeof(data->s1) + sizeof(data->n);
     return data;
 }
 
