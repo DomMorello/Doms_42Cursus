8f320ae6938dd25d593b36581466383d23bb8291
donglee <zuzudnf@gmail.com>
2021-04-12 21:32:08 +0900

    ex01 done?

--- CPP_Module/CPP_module_06/ex01/main.cpp
+++ CPP_Module/CPP_module_06/ex01/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <string.h>
 
 struct Data
 {
@@ -9,7 +10,7 @@ struct Data
     std::string s2;
 };
 
-void serialize(void) {
+void *serialize(void) {
     const char alphanum[] = "0123456789abcdefghijklmnopqrstuvwxyz";
     std::string s1("");
     std::string s2("");
@@ -22,13 +23,32 @@ void serialize(void) {
         s1 += alphanum[rand() % (strlen(alphanum) - 1)];
         s2 += alphanum[rand() % (strlen(alphanum) - 1)];
     }
+    char *ret = new char[sizeof(s1) + sizeof(randInt) + sizeof(s2)];
+    std::cout << "s1 : " << s1 << std::endl;
+    std::cout << "n : " << randInt << std::endl;
+    std::cout << "s2 : " << s2 << std::endl;
+
+    memcpy(ret, &s1, sizeof(s1));
+    memcpy(ret + sizeof(s1), &randInt, sizeof(randInt));
+    memcpy(ret + sizeof(s1), &s2, sizeof(s2));
+    return ret;
 }
 
-// Data* deserialize(void *raw) {
-    
-// }
+Data* deserialize(void *raw) {
+    Data *data = new Data;
+
+    data->s1 = reinterpret_cast<char*>(raw);
+    data->n = *reinterpret_cast<int*>(reinterpret_cast<char*>(raw) + sizeof(data->s1));
+    data->s2 = reinterpret_cast<char*>(raw) + sizeof(data->s1) + sizeof(data->s2);
+    return data;
+}
 
 int main()
 {
-    serialize();
+    char *serial = reinterpret_cast<char*>(serialize());
+    Data *deserial = deserialize(serial);
+
+    std::cout << deserial->s1 << std::endl;
+    std::cout << deserial->n << std::endl;
+    std::cout << deserial->s2 << std::endl;  
 }
