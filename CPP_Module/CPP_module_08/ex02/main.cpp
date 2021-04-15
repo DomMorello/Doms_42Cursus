464b4d9b1d1a3bad5127acb619ce6a4298402656
donglee <zuzudnf@gmail.com>
2021-04-15 14:53:12 +0900

    inlcude cpp file and add main test function

--- CPP_Module/CPP_module_08/ex02/main.cpp
+++ CPP_Module/CPP_module_08/ex02/main.cpp
@@ -1,4 +1,5 @@
 #include "mutantstack.hpp"
+#include "mutantstack.cpp"
 
 int main()
 {
@@ -7,18 +8,18 @@ int main()
     mstack.push(5);
     mstack.push(17);
     
-    std::cout << mstack.top() << std::endl;
+    std::cout << "top: " << mstack.top() << std::endl;
     
     mstack.pop();
     
-    std::cout << mstack.size() << std::endl;
+    std::cout << "size: " << mstack.size() << std::endl;
     
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
     
+    //전체 출력
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
     
@@ -30,5 +31,26 @@ int main()
         ++it;
     }
     std::stack<int> s(mstack);
+    std::cout << "copied size: " << s.size() << std::endl;
+
+    std::cout << "-------------------------" << std::endl;
+
+    MutantStack<std::string> ms;
+    ms.push("donglee");
+    ms.push("domas");
+    ms.push("morello");
+    ms.push("hello");
+    ms.push("world");
+
+    ms.pop();
+    ms.pop();
+
+    MutantStack<std::string>::iterator it2 = ms.begin();
+    MutantStack<std::string>::iterator ite2 = ms.end();
+    while (it2 != ite2)
+    {
+        std::cout << *it2 << std::endl;
+        ++it2;
+    }
     return 0;
 }
--- CPP_Module/CPP_module_08/ex02/mutantstack.hpp
+++ CPP_Module/CPP_module_08/ex02/mutantstack.hpp
@@ -2,6 +2,7 @@
 #define MUTANTSTACK_HPP
 
 #include <iostream>
+#include <string>
 #include <stack>
 #include <deque>
 
