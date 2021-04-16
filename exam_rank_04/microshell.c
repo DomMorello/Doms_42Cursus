4662149c6282a3103e8d6d646b760e1962d9e0d6
Donghyeon Lee <zuzudnf@gmail.com>
2021-04-16 15:29:30 +0900

    exam practice

---
+++ exam_rank_04/microshell.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CD_BAD "error: cd: bas arguments\n"
+#define CD_FAIL "error: cd: cannot change direcotry to "
+#define SYS_ERR "error: fatal\n"
+#define EXEC_ERR "error: cannot execute "
+
+int main(int argc, char **argv, char **envp)
+{
+	int fd[2];
+
+	
+	return 0;
+}
