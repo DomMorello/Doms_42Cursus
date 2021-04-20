f17131f7db05f6f30d3e14d648100b48789025c0
Donghyeon Lee <zuzudnf@gmail.com>
2021-04-20 16:39:41 +0900

    exam rank 04 done

--- exam_rank_04/microshell.c
+++ exam_rank_04/microshell.c
@@ -2,92 +2,92 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define CD_ERR "error: cd: bad arguments\n"
-#define CD_ERR2 "error: cd: cannot change direcotry to "
-#define SYS_ERR "error: fatal\n"
+#define	CD_ERR	"error: cd: bad arguments\n"
+#define CD_ERR2	"error: cd: cannot change directory to "
+#define	SYS_ERR	"error: fatal\n"
 #define EXEC_ERR "error: cannot execute "
 
 int ft_strlen(char *s)
 {
-	int len = 0;
+    int len = 0;
 
-	while (s[len])
-		len++;
-	return len;
+    while (s[len])
+        len++;
+    return len;
 }
 
 int err(char *s)
 {
-	write(2, s, ft_strlen(s));
-	return 1;
-}
-
-int is_pipe(char **argv)
-{
-	int i = 0;
-
-	while (argv[i])
-	{
-		if (!stncmp(argv[i], "|", 2))
-		{
-			argv[i] = NULL;
-			return 1;
-		}
-		else if (!strncmp(argv[i], ";", 2))
-		{
-			argv[i] = NULL;
-			return 0;
-		}
-		i++;
-	}
-	return 0;
+    write(2, s, ft_strlen(s));
+    return 1;
 }
 
 int ft_argvlen(char **argv)
 {
-	int i = 0;
-	
-	while (argv[i])
-		i++;
-	return i;
+    int len = 0;
+
+    while (argv[len])
+        len++;
+    return len;
+}
+
+int is_pipe(char **argv)
+{
+    int i  = 0;
+
+    while (argv[i])
+    {
+        if (!strncmp(argv[i], "|", 2))
+        {
+            argv[i] = NULL;
+            return 1;
+        }
+        else if (!strncmp(argv[i], ";", 2))
+        {
+            argv[i] = NULL;
+            return 0;
+        }
+        i++;
+    }
+    return 0;
 }
 
 void ft_cd(char **argv)
 {
-	int i = ft_argvlen(argv);
+    int i = ft_argvlen(argv);
 
-	if (i != 2)
-		err(CD_ERR);
-	else if (chdir(argv[1]) < 0)
-	{
-		err(CD_ERR2);
-		err(argv[1]);
-		err("\n");
-	}
+    if (i != 2)
+        err(CD_ERR);
+    else if (chdir(argv[1]) < 0)
+    {
+        err(CD_ERR2);
+        err(argv[1]);
+        err("\n");
+    }
 }
 
-void shell(int argc, char **argv, char **envp, int prev, int *fd_prev)
+void	shell(int argc, char **argv, char **envp, int prev, int *fd_prev)
 {
-	pid_t pid;
-	int fd_next[2];
-	int next, status, i;
+	pid_t	pid;
+	int		fd_next[2];
+	int		next, status, i;
 
 	if (argc <= 0)
 		return ;
 	next = is_pipe(argv);
 	if (argv[0])
 	{
-		if (next && pipe(fd_next) < 0)
+		if (next && pipe(fd_next)<0)
 			exit(err(SYS_ERR));
 		if (!strncmp(argv[0], "cd", 3))
-			fd_cd(argv);
+			ft_cd(argv);
 		else if ((pid = fork()) < 0)
 			exit(err(SYS_ERR));
 		else if (!pid)
 		{
-			if (prev && dup2(fd_prev[0], 0) < 0)
+			if (prev && dup2(fd_prev[0], 0)<0)
 				exit(2);
-			if (next && dup2(fd_prev[1], 1) < 0)
+			if (next && dup2(fd_next[1], 1)<0)
 				exit(2);
 			if (execve(argv[0], argv, envp) < 0)
 			{
@@ -110,15 +110,14 @@ void shell(int argc, char **argv, char **envp, int prev, int *fd_prev)
 		}
 	}
 	i = ft_argvlen(argv);
-	shell(argc - i - 1, argv + i + 1, envp, next, fd_next);
+	shell(argc-i-1, argv+i+1, envp, next, fd_next);
 }
 
 int main(int argc, char **argv, char **envp)
 {
-	int fd_prev[2];
-
-	fd_prev[0] = STDIN_FILENO;
-	fd_prev[1] = STDOUT_FILENO;
-	shell(argc - 1, argv + 1, envp, 0, fd_prev);
-	return 0;
+    int fd_prev[2];
+    fd_prev[0] = 0;
+    fd_prev[1] = 1;
+    shell(argc - 1, argv + 1, envp, 0, fd_prev);
+    return 0;
 }
